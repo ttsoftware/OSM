f34fb68ed361116c87e3914e73a8b368ee6719ec
bajinai <bajinalphi@gmail.com>
2014-02-19 22:15:32 +0100

    fixed run.sh

--- buenos-ex2/run.sh
+++ buenos-ex2/run.sh
@@ -4,8 +4,7 @@ make clean
 make
 make -C tests/
 rm fyams.harddisk
-#util/tfstool create fyams.harddisk 2048 arkimedes
-util/tfstool delete fyams.harddisk exec
+util/tfstool create fyams.harddisk 2048 arkimedes
 util/tfstool write fyams.harddisk tests/exec exec
 fyams-sim buenos 'initprog=[arkimedes]exec'
 
